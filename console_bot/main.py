aa08fd1e13b3e4b79f0c2973ce570915066328c1
kravchenmd <kravchenmd@gmail.com>
2022-06-29 00:42:34 +0300

    Fix

--- console_bot/main.py
+++ console_bot/main.py
@@ -1,14 +1,18 @@
-import keyboard
 import os
 import pickle
 import sys
 import time
 
+import keyboard
+import psutil
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import WordCompleter
+
 from address_book import address_book_functions as f
-from notes import notes as n
 from address_book.address_book_class import AddressBook
-from notes.notes import Notes
 from command_handling import handle_cmd
+from notes import notes as n
+from notes.notes import Notes
 
 
 def main():
@@ -16,6 +20,13 @@ def main():
     contacts = None
     notes = None
     arg = None  # argument to pass to `handle_cmd()` depending on the mode
+    terminal_run = False  # for prompt_toolkit
+
+    # Check if program is running PyCharm or cmd, bash, etc. for prompt_toolkit
+    shells = {"cmd.exe", "bash.exe", "powershell.exe", "WindowsTerminal.exe"}
+    parents = {parent.name() for parent in psutil.Process().parents()}
+    if bool(parents & shells):
+        terminal_run = True
 
     start_message = "*** Console bot project ***\n" \
                     "***  Team #3 - PyStars  ***\n"
@@ -26,17 +37,18 @@ def main():
                     "\t1   - Address book mode\n" \
                     "\t2   - Note mode\n" \
                     "\t3   - Sorting of a folder\n" \
-                    "Press needed number..."
+                    "Input number and press Enter, or press Esc to exit..."
     print(start_message)
 
     while not mode:
         if keyboard.is_pressed('Esc'):
             print("\nGoodbye!")
             sys.exit()  # exit program (to prevent going to the next while)
+
         if keyboard.is_pressed('1'):
+            input()
             mode = 'AddressBook'
             contacts = AddressBook()
-            arg = contacts
 
             # Load contacts from file
             data, result = contacts.load_from()
@@ -44,9 +56,14 @@ def main():
                 contacts.data = data
             print(f"\n{result}")
 
-            print("\nMode: Address book")
+            if terminal_run:
+                command_completer = WordCompleter(
+                    ['help', 'exit', 'hello', 'add_contact', 'remove_contact', 'change_phone', 'remove_phone',
+                     'show_email', 'change_email', 'remove_email', 'show_phones', 'show_all', 'edit_birthday',
+                     'days_to_birthday' 'birthday_in', 'save', 'load', 'find_contact'])
 
         if keyboard.is_pressed('2'):
+            input()
             mode = 'Notes'
 
             path = 'database/notes_db.bin'
@@ -61,14 +78,18 @@ def main():
             print("\nMode: Notes")
 
         if keyboard.is_pressed('3'):
+            input()
             mode = 'Sorting'
             print("\nMode: Sorting")
 
     print("***For help type `help` command**\n")
 
     while True:
+        # text = prompt('Enter HTML: ', completer=html_completer)
         command = None
-        # Check if command is not empty
+
+        if terminal_run:
+            command = prompt('Enter command: ', completer=command_completer)
         while not command:
             command = input('Enter command: ')
 
